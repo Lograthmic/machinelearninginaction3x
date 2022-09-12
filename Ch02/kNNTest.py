2d8ad29f8aa5a7d029b038be5deea92b78a0ff2d
Logarithmic <z1322163049@outlook.com>
2022-09-12 23:02:34 +0800

    更新第二章笔记

--- Ch02/kNNTest.py
+++ Ch02/kNNTest.py
@@ -7,16 +7,28 @@ from numpy import *
 import matplotlib
 import matplotlib.pyplot as plt
 
-from Ch02 import kNN
+import kNN
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-datingDataMat,datingLabels = kNN.file2matrix('datingTestSet.txt')
+ax = fig.add_subplot(311)
+datingDataMat,datingLabels = kNN.file2matrix('./Ch02/datingTestSet.txt')
 # ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
 ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
 ax.axis([-2,25,-0.2,2.0])
-plt.xlabel('Percentage of Time Spent Playing Video Games')
-plt.ylabel('Liters of Ice Cream Consumed Per Week')
+plt.xlabel('玩游戏所耗时间百分比')
+plt.ylabel('每周消费冰淇淋公升数')
+
+ax = fig.add_subplot(312)
+ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.xlabel('每年获取的飞行常客里程数')
+plt.ylabel('玩游戏所耗时间百分比')
+
+ax = fig.add_subplot(313)
+ax.scatter(datingDataMat[:,0], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.xlabel('每年获取的飞行常客里程数')
+plt.ylabel('每周消费冰淇淋公升数')
+
+
 plt.show()
 
 #Test dating class
